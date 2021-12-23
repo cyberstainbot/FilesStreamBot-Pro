5ac1421cc2976766f595dc990481be2cd9ffded0
flaskvol <88590134+flaskvol@users.noreply.github.com>
2021-12-23 13:59:01 +0530

    credit

--- Code_X_Mania/bot/plugins/start.py
+++ Code_X_Mania/bot/plugins/start.py
@@ -253,7 +253,7 @@ async def help_handler(bot, message):
         except Exception:
             await bot.send_message(
                 chat_id=message.chat.id,
-                text="__Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ__ ADARSH GOEL](https://t.me/cyberstainbot).",
+                text="__Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ__ CyberStainBot](https://t.me/cyberstainbot).",
                 parse_mode="markdown",
                 disable_web_page_preview=True)
             return
